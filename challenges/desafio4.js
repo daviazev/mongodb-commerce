ee710d186034bfea6ecdefb4e6d818c9c56d8eb3
daviazevdo <azvd.davi@gmail.com>
2023-02-07 16:30:26 -0300

    feat: requisito 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,13 @@
+// prettier-ignore
+db.produtos
+  .find(
+    {
+      vendidos: { $gt: 50, $lt: 100 },
+    },
+    {
+      nome: true,
+      vendidos: true,
+      _id: false,
+    },
+  )
+  .sort({ vendidos: 1 });
